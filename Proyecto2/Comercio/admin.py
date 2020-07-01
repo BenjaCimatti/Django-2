e550f4bc3b12a7613ea68d093dfb36033e02e9f0
BenjaCimatti <benja.cimatti@gmail.com>
2020-07-01 12:09:44 -0300

    ProductoAdmin y ProveedorAdmin agregados

--- Proyecto2/Comercio/admin.py
+++ Proyecto2/Comercio/admin.py
@@ -6,6 +6,23 @@ class ClienteAdmin(admin.ModelAdmin):
     list_display = ['RUT','nombre','telefono']
     list_display_links = ['RUT','nombre','telefono']
 
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ['nombre','proveedor','precio','stock']
+    list_display_links = ['nombre','proveedor','precio','stock']
+    fieldsets = (
+        ("Descripcion", {
+            'fields':('nombre','categoria','proveedor',)
+        }),
+        ('Variables', {
+            'fields':('precio','stock',)
+        }),
+    )
+
+class ProveedorAdmin(admin.ModelAdmin):
+    search_fields = ['nombre','RUT']
+    list_display = ['nombre','telefono','direccion']
+    list_display_links = ['nombre','telefono','direccion']
+
 # Register your models here.
 admin.site.register(Categoria)
 admin.site.register(Ciudad)
@@ -13,6 +30,6 @@ admin.site.register(Comuna)
 admin.site.register(Direccion)
 admin.site.register(Cliente, ClienteAdmin)
 admin.site.register(Venta)
-admin.site.register(Proveedor)
-admin.site.register(Producto)
+admin.site.register(Proveedor, ProveedorAdmin)
+admin.site.register(Producto, ProductoAdmin)
 admin.site.register(Detalle)
